7931d033e1176ed718fb41b3761144049c85533a
ninesky00 <jiangzho@gmail.com>
2020-10-07 15:03:37 -0500

    add iterations 1, 2, 3, and 4 to restaurant.rb

---
+++ lib/restaurant.rb
@@ -0,0 +1,44 @@
+class Restaurant
+    attr_reader :name, :dishes
+    def initialize(opening_time = "11:30", name)
+        @opening_time = opening_time
+        @name = name 
+        @dishes =[]
+    end
+    # would love to refractor how to break up the time input string so code doesn't look like below
+    def opening_time
+        opening_time = Time.new(2020, 10, 07, @opening_time[0..1], @opening_time[3..4])
+        opening_time.strftime("%k:%M")
+    end
+
+    def closing_time(hours)
+        closing_time = Time.new(2020, 10, 07, @opening_time) + hours*60*60
+        closing_time.strftime("%k:%M")
+    end
+
+    def add_dish(dish)
+        @dishes << dish
+    end
+
+    def open_for_lunch?
+        if Time.new(2020, 10, 07, @opening_time[0..1], @opening_time[3..4]) < Time.new(2020, 10, 07, 12)
+            true
+        else
+            false
+        end
+    end
+
+    def menu_dish_names
+        menu_dish_names =[]
+        @dishes.each do |dish|
+            menu_dish_names << dish.upcase
+        end
+        menu_dish_names
+    end
+
+    def announce_closing_time(hours)
+        closing_time = Time.new(2020, 10, 07, @opening_time) + hours*60*60
+        closing_time.strftime("%l:%M")
+        "#@name will be closing at #{closing_time.strftime('%l:%M%p')}"
+    end
+end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -11,21 +11,21 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_opening_time
-    skip
+    #skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal '10:00', restaurant.opening_time
   end
 
   def test_it_has_name
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal 'Fuel Cafe', restaurant.name
   end
 
   def test_it_has_dishes
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal [], restaurant.dishes
@@ -33,7 +33,7 @@ class RestaurantTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_has_closing_time
-    skip
+    # skip
     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
     restaurant2 = Restaurant.new('16:00', 'Il Poggio')
 
@@ -42,7 +42,7 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_can_add_dishes
-    skip
+    #skip
     restaurant = Restaurant.new('16:00', 'Il Poggio')
 
     restaurant.add_dish('Burrata')
@@ -50,5 +50,31 @@ class RestaurantTest < Minitest::Test
     restaurant.add_dish('Ravioli')
 
     assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], restaurant.dishes
+  end
+
+  def test_it_is_open_for_lunch
+    #skip
+    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+
+    assert restaurant1.open_for_lunch?
+    refute restaurant2.open_for_lunch?
+  end
+
+  def test_can_display_menu_dish_names
+    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+    restaurant2.add_dish('Burrata')
+    restaurant2.add_dish('Pizzetta')
+    restaurant2.add_dish('Ravioli')
+
+    assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], restaurant2.menu_dish_names
+  end
+
+  def test_announce_closing_time
+    restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
+    restaurant2 = Restaurant.new('16:00', 'Il Posto')
+
+    assert_equal "Fuel Cafe will be closing at 11:00AM", restaurant1.announce_closing_time(5)
+    assert_equal "Il Posto will be closing at 11:00PM", restaurant2.announce_closing_time(7)
   end
 end
